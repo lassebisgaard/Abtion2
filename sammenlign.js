7294d664a2edd6da47df6ea0668253a2f03b7b44
lassebisgaard <Lasse_4321@hotmail.com>
2023-06-13 20:58:11 +0200

    Done

--- sammenlign.js
+++ sammenlign.js
@@ -10,10 +10,10 @@ benzinBillede.style.display = "none";
 
 // Lyt efter ændringer i elbil inputfeltet
 elbilInput.addEventListener("input", function() {
-  var searchText = elbilInput.value.toLowerCase();
+  var searchText = elbilInput.value.toLowerCase().trim(); // Konverter til lowercase og fjern mellemrum
 
-  // Check om søgeteksten starter med "elbil"
-  if (searchText.startsWith("elbil")) {
+  // Check om søgeteksten indeholder "mercedes - eqe 300 aut."
+  if (searchText.includes("mercedes - eqe 300 aut.")) {
     // Vis elbilbilledet
     elbilBillede.style.display = "block";
   } else {
@@ -24,10 +24,10 @@ elbilInput.addEventListener("input", function() {
 
 // Lyt efter ændringer i bil inputfeltet
 bilInput.addEventListener("input", function() {
-  var searchText = bilInput.value.toLowerCase();
+  var searchText = bilInput.value.toLowerCase().trim();
 
-  // Check om søgeteksten starter med "bil"
-  if (searchText.startsWith("bil")) {
+  // Check om søgeteksten indeholder "mercedes - c 220 d aut. advantage"
+  if (searchText.includes("mercedes - c 220 d aut. advantage")) {
     // Vis benzinbilledet
     benzinBillede.style.display = "block";
   } else {
@@ -44,11 +44,11 @@ bilInput.addEventListener("input", updateSammenlignBillede);
 
 // Funktion til at opdatere sammenligningsbilledet
 function updateSammenlignBillede() {
-  var elbilSearchText = elbilInput.value.toLowerCase();
-  var bilSearchText = bilInput.value.toLowerCase();
+  var elbilSearchText = elbilInput.value.toLowerCase().trim(); // Konverter til lowercase og fjern mellemrum
+  var bilSearchText = bilInput.value.toLowerCase().trim();
 
-  // Tjek om søgeteksten starter med "elbil" og "bil"
-  if (elbilSearchText.startsWith("elbil") && bilSearchText.startsWith("bil")) {
+  // Tjek om søgeteksten indeholder "mercedes - eqe 300 aut." og "mercedes - c 220 d aut. advantage"
+  if (elbilSearchText.includes("mercedes - eqe 300 aut.") && bilSearchText.includes("mercedes - c 220 d aut. advantage")) {
     sammenlignBillede.src = "billeder/tabel1.png"; // Opdater billedets kilde
   } else {
     sammenlignBillede.src = "billeder/tabel.png"; // Gendan standardbilledet
